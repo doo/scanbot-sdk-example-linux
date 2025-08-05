2bda8125e48c35581cf63bc87e52ecc96158192d
Kyrylo <kyrylo@scanbot.io>
2025-08-05 18:43:12 +0200

    C Wrapper Barcode Live Example

---
+++ examples/c/FindScanbotSDK.cmake
@@ -0,0 +1,41 @@
+include(ExternalProject)
+include(FindPackageHandleStandardArgs)
+
+if(NOT SCANBOTSDK_DIR)
+message(FATAL_ERROR "SCANBOTSDK_DIR not set")
+endif()
+
+if (NOT EXISTS ${SCANBOTSDK_DIR})
+
+  find_program(WGET_PATH wget REQUIRED)
+
+  message(STATUS "Downloading ScanbotSDK ${SCANBOTSDK_VERSION} to ${SCANBOTSDK_DIR}")
+
+  if ("${CMAKE_HOST_SYSTEM_PROCESSOR}" MATCHES ".*arm.*" OR "${CMAKE_HOST_SYSTEM_PROCESSOR}" MATCHES ".*aarch64.*")
+    set(SCANBOTSDK_ARCHITECTURE "aarch64")
+  else ()
+    set(SCANBOTSDK_ARCHITECTURE "x86_64")
+  endif ()
+  string(TOLOWER "${CMAKE_HOST_SYSTEM_NAME}-${SCANBOTSDK_ARCHITECTURE}" PLATFORM_ID)
+  set(URL "https://scanbotsdk-deployment.s3.amazonaws.com/ScanbotSDK-Linux/${SCANBOTSDK_API_TOKEN}/scanbotsdk-${SCANBOTSDK_VERSION}-${PLATFORM_ID}.tar.gz")
+  execute_process(
+    COMMAND ${WGET_PATH} "${URL}"
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+  )
+  execute_process(
+    COMMAND tar -xf scanbotsdk-${SCANBOTSDK_VERSION}-${PLATFORM_ID}.tar.gz
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+  )
+endif()
+
+find_library(ScanbotSDK_LIBS
+  NAMES libscanbotsdk.so
+  HINTS ${SCANBOTSDK_DIR}/lib/
+  )
+
+find_path(ScanbotSDK_INCLUDE_DIRS
+  NAMES ScanbotSDK.h
+  HINTS ${SCANBOTSDK_DIR}/include/)
+
+find_package_handle_standard_args(ScanbotSDK
+  REQUIRED_VARS ScanbotSDK_LIBS ScanbotSDK_INCLUDE_DIRS)
